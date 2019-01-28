87dc6e93ea5479bf74220f56051928560e285bab
ROHAN PRASAD <34542787+rohap97@users.noreply.github.com>
2019-01-28 21:51:38 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/paginglibrary/Api.java
@@ -0,0 +1,16 @@
+package com.example.android.paginglibrary;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface Api {
+
+    @GET("answers")
+    Call<StackApiResponse> getAnswers(
+            @Query("page") int page,
+            @Query("pagesize") int size,
+            @Query("site") String site
+    );
+
+}
